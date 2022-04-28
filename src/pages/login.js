56793f7a4e777d76a13ae35cd0c8a06ee6b3098c
aline <aline.rosa.cruz@gmail.com>
2022-04-27 22:09:59 -0300

    funcionalidade de login com o google mas o template string do spa sumiu

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 import "../lib/config-firebase.js";
+import { loginGoogle } from "../lib/auth-firebase.js";
 //import "https://www.gstatic.com/firebasejs/9.6.11/firebase-auth.js";
 
 export default () => {
@@ -10,8 +11,9 @@ export default () => {
     <input type="password" name="password" class="password-input input-users" placeholder="Insera uma senha" requerid /><br>
   <a href="" > <p class='reset-password'>Esqueceu a sua senha?</p></a><br>
   <br><button class='btn-submit' type="submit">Entrar</button><br>
-  <button class="btn-google">Entrar com o Google
+  <button class="btn-google" id="buttonGoogle">Entrar com o Google
   </button><br>
+  <p id="message" class="successMessage errorMessage"></p>
   `;
 
 containerLogin.innerHTML = templateLogin;
@@ -19,3 +21,19 @@ containerLogin.innerHTML = templateLogin;
 return containerLogin;
 
 }
+
+const loginButtonGoogle = containerLogin.querySelector("#buttonGoogle");
+const messageSuccess = containerLogin.querySelector(".successMessage");
+//const messageError = containerLogin.querySelector('.errorMessage');
+
+
+loginButtonGoogle.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginGoogle().then((result) => {
+      messageSuccess.innerHTML = "Login google feito com sucesso!";
+      //window.location.hash = "#feed"; //substituir mensagem quando criar pagina de timeline com posts 
+    })
+    .catch((error) => {
+      //messageError.innerHTML = "Login não deu certo, tente novamente!";
+    });
+});
